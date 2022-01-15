d869aeb1774636868342209e13850dd884826566
paulmski <paulmski@gmail.com>
2022-01-15 15:20:20 -0500

    Added getWarmth function

--- Bulb.py
+++ Bulb.py
@@ -31,5 +31,11 @@ class Bulb(SmartDevice):
     }
         r = self._api.POST(url='/v1.0/devices/' + self._id + '/commands', body=command)
         return r
+
+        
+
+    def getWarmth(self):
+        return int(self.getStatus()['result'][3]['value'])
     def getBrightness(self):
         return int(self.getStatus()['result'][2]['value'])
+
