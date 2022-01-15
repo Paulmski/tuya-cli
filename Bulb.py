90f988f4aefa39865078ad0887a0b65fafcd3f09
paulmski <paulmski@gmail.com>
2022-01-15 15:36:34 -0500

    Added set and get Colour functions

--- Bulb.py
+++ Bulb.py
@@ -32,10 +32,26 @@ class Bulb(SmartDevice):
         r = self._api.POST(url='/v1.0/devices/' + self._id + '/commands', body=command)
         return r
 
+    def setColour(self, h, s, v):
+        command = {
+        "commands": [
+            {"code": "colour_data_v2",
+             "value": {
+                 "h": h, "s": s, "v": v
+             }
+             }
+        ]
+    }
+
+        r = self._api.POST(url='/v1.0/devices/' + self._id + '/commands', body=command)
+        return r
+
+
         
 
+    def getColour(self):
+        return self.getStatus()['result'][4]['value']
     def getWarmth(self):
         return int(self.getStatus()['result'][3]['value'])
     def getBrightness(self):
         return int(self.getStatus()['result'][2]['value'])
-
